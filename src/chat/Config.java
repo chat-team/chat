dd19b9b9ded9e596e725412ce03390abf8a0cabf
He Tao <hetao@buaa.edu.cn>
2015-12-15 21:54:05 +0800

    Add properties file to .gitignore.

--- src/chat/Config.java
+++ src/chat/Config.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Config {
     private static Properties prop = new Properties();
     static {
-        InputStream in = Config.class.getResourceAsStream("/chat.properities");
+        InputStream in = Config.class.getResourceAsStream("/chat.properties");
         try {
             prop.load(in);
             in.close();
