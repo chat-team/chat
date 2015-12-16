41d6415024b3497ff6d6d50e3e43b9107e6d6764
Zhao Dongfang <615328619@qq.com>
2015-12-16 16:58:40 +0800

    update Query

---
+++ src/chat/QueryGroup.java
@@ -0,0 +1,67 @@
+package chat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+/**
+ * Created by Dongfang on 2015/12/16.
+ */
+@WebServlet(name = "QueryGroup")
+public class QueryGroup extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        ReqReader reader = new ReqReader(request.getInputStream());
+        ResWriter writer = new ResWriter(response.getOutputStream());
+        String username = reader.getString("userid");
+
+        if (username == "" ) {
+            writer.add("status", "failed").write();
+            return;
+        }
+
+        DatabaseConnection dbConn = new DatabaseConnection();
+        Connection conn = dbConn.getConnection();
+        String sql = "SELECT groupid FROM group_belong WHERE userid = ?";
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setObject(1, username);
+            rs = ps.executeQuery();
+            ArrayList<String> array = new ArrayList<String>();
+            while (rs.next()) {
+                array.add(rs.getString(1));
+            }
+            writer.add("status", "success");
+            writer.add("group", array).write();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
