85136d1d7b99ddd8294dec18c1307f1bea39a005
Zhao Dongfang <615328619@qq.com>
2015-12-22 14:40:23 +0800

    Update Find friend group chatroom

--- src/chat/FindGroup.java
+++ src/chat/FindGroup.java
@@ -10,19 +10,22 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by Dongfang on 2015/12/22.
  */
-@WebServlet(name = "JoinGroup", urlPatterns = {"/joingroup"})
-public class JoinGroup extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+@WebServlet(name = "FindGroup", urlPatterns = {"/findgroup"})
+public class FindGroup extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         ReqReader reader = new ReqReader(request.getInputStream());
         ResWriter writer = new ResWriter(response.getOutputStream());
 
-        String username, targetid;
+        String username;
+
         HttpSession session = request.getSession();
         if (session.getAttribute("userid") != null) {
             username = (String)session.getAttribute("userid");
@@ -32,28 +35,37 @@ public class JoinGroup extends HttpServlet {
             response.setStatus(401);
             return; // no valid userid.
         }
-        targetid = reader.getString("targetid");
 
-        if (username == "" || targetid == "") {
+        if (username == "" ) {
             writer.add("status", "failed").write();
             return;
         }
 
         DatabaseConnection dbConn = new DatabaseConnection();
         Connection conn = dbConn.getConnection();
-        String sql = "insert into group_belong (groupid, userid) values (?, ?)";
+        String sql = "SELECT groupid, groupname FROM group_info";
         PreparedStatement ps = null;
+        ResultSet rs = null;
         try {
             ps = conn.prepareStatement(sql);
-            ps.setString(1, targetid);
-            ps.setString(2, username);
-            ps.executeUpdate();
-            writer.add("status", "success").write();
+            rs = ps.executeQuery();
+            ArrayList<Map<String, String>> array = new ArrayList<>();
+            while (rs.next()) {
+                Map<String, String> m = new TreeMap<>();
+                m.put("groupid", rs.getString("groupid"));
+                m.put("groupname", rs.getString("groupname"));
+                array.add(m);
+            }
+            writer.add("status", "success");
+            writer.add("group", array).write();
         } catch(Exception e) {
             e.printStackTrace();
         }
         finally {
             try {
+                if (rs != null) {
+                    rs.close();
+                }
                 if (ps != null) {
                     ps.close();
                 }
@@ -64,6 +76,7 @@ public class JoinGroup extends HttpServlet {
                 e.printStackTrace();
             }
         }
+
     }
 
 }
